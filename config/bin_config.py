7681eac1055c95b46cb704718c51ab4d491c880e
Trudigard <christina.brodowsky@gmail.com>
2025-04-14 17:57:34 +0200

    introduce objects for bins, ranges, and species

--- config/bin_config.py
+++ config/bin_config.py
@@ -16,196 +16,99 @@ def parse_range(config, section, option):
 	parsed_range = [float(i) for i in range_str]
 	return parsed_range
 
-pi = np.pi
-
-config = configparser.ConfigParser()
-config.read('config.ini')
-
 # ==============================================================================
-# Dictionary for species information
-# If species are added, add them here
-# "range_bounds", "range_idx", and "enabled" are read from the config.ini file
-# everything else is set here
+# Types for bin, range and species information
 # ==============================================================================
+class AerosolSpecies:
+	def __init__(self, config, species):
+		self.active = config.getboolean(species, 'active', fallback=False)
+		self.short_name = config.get(species, 'short_name')
+		self.long_name = config.get(species, 'long_name')
+		self.composition = config.get(species, 'composition')
+		self.soluble = config.getboolean(species, 'soluble')
+		self.range_bnds = parse_range(config, species, 'range_bounds')
+		self.range_idx = []
+	def check_range_bnds(self, range_bnds):
+		if any(i not in range_bnds for i in self.range_bnds):
+			sys.exit('ERROR: Species range bound not equal to range bounds')
+	def get_range_idx(self, range_bnds):
+		# adjust species bounds to range bounds
+		idx0 = (np.abs(range_bnds - self.range_bnds[0])).argmin()
+		idx1 = (np.abs(range_bnds - self.range_bnds[1])).argmin()
+		self.range_bnds = [range_bnds[idx0], range_bnds[idx1]]
+		# get range indices for each species
+		self.range_idx.extend([i for i in range(1, len(range_bnds))
+								 if range_bnds[i] > self.range_bnds[0]
+								 and range_bnds[i] <= self.range_bnds[1]])
 
-species_info = {
-	'organic': {
-    	'short_name': 'OA',
-		'long_name': 'Organic aerosol',
-		'range_bounds': (0,0),
-		'range_idx': [],
-		'composition': 'C',
-		'enabled': False,
-  		'soluble': True
-	},
-	'dust': {
-    	'short_name': 'DU',
-		'long_name': 'Dust aerosol',
-		'range_bounds': (0,0),
-		'range_idx': [],
-		'composition': 'AlSiO5',
-		'enabled': False,
-  		'soluble': True
-	},
-	'sulfate': {
-		'short_name': 'SO4',
-		'long_name': 'Sulfate aerosol',
-		'range_bounds': (0,0),
-		'range_idx': [],
-		'composition': 'NH4SO4',
-		'enabled': False,
-  		'soluble': True
-	},
-	'blackcarbon': {
-		'short_name': 'BC',
-		'long_name': 'Black Carbon aerosol',
-		'range_bounds': (0,0),
-		'range_idx': [],
-		'composition': 'C',
-		'enabled': False,
-  		'soluble': True
-	},
-	'blackcarbon_insoluble': {
-		'short_name': 'BC_is',
-		'long_name': 'Insoluble Black Carbon aerosol',
-		'range_bounds': (0,0),
-		'range_idx': [],
-		'composition': 'C',
-		'enabled': False,
-  		'soluble': False
-	},
-	'seasalt': {
-		'short_name': 'SS',
-		'long_name': 'Seasalt aerosol',
-		'range_bounds': (0,0),
-		'range_idx': [],
-		'composition': 'NaCl',
-		'enabled': False,
-  		'soluble': True
-	},
-	'nitrate': {
-		'short_name': 'NO3',
-		'long_name': 'Nitrate aerosol',
-		'range_bounds': (0,0),
-		'range_idx': [],
-		'composition': 'NO3',
-		'enabled': False,
-  		'soluble': True
-	}
-}
+# bin settings, maybe add 'method' to allow for other than
+# volume ratio. e.g. 'custom' -> user defined bin bounds
+class BinSpecs:
+	def __init__(self, config):
+		self.N = config.getint('BIN SPECS', 'nbin')
+		self.r_1 = config.getfloat('BIN SPECS', 'radius_1')
+		self.r_N = config.getfloat('BIN SPECS', 'radius_N')
+		self.r = None
+		self.r_bnds = None
+	def calc_bins(self):
+		# Bin calculations - Volume ratio approach
+		V_rat = (self.r_N / self.r_1) ** (3 / (self.N-1)) 		# calculate volume ratio
+		v_0 = 3/4 * np.pi * (self.r_1) ** 3 # # calculate smallest volume
+		v = v_0 * V_rat ** np.arange(self.N) # calculate all volumes for bins
+		v_lo = (2*v) / (1+V_rat) # lower radius bounds
+		v_hi = V_rat*v_lo[-1] # upper bnd for largest bin
+		v_bnds = np.append(v_lo, v_hi) # append to get volume bounds
+
+		self.r = (v*4/(3*np.pi))**(1/3) # calculate radii
+		self.r_bnds = (v_bnds*4/(3*np.pi))**(1/3) # calculate radius bounds from volume
+
+class RangeSpecs:
+	def __init__(self, config):
+		self.ranges = config.getboolean('RANGE SPECS', 'ranges')
+		self.range_bnds = np.asarray(parse_range(config, 'RANGE SPECS', 'range_bounds'))
+	def adjust_range_bnds(self, r_bnds):
+		if self.ranges:
+			self.range_bnds[0] = r_bnds[0]
+			self.range_bnds[-1] = r_bnds[-1]
+			for i in range(1, len(self.range_bnds)-1):
+				idx = (np.abs(r_bnds - self.range_bnds[i])).argmin()
+				self.range_bnds[i] = r_bnds[idx]
+		else:
+			# If no ranges (classic sectional scheme), set range bounds equal to bin bounds
+			self.range_bnds = r_bnds
 
 # ==============================================================================
 # Read input from config file
 # ==============================================================================
-
-# bin settings
-N = config.getint('BIN SPECS', 'nbin')
-r_1 = config.getfloat('BIN SPECS', 'radius_1')
-r_N = config.getfloat('BIN SPECS', 'radius_N')
-
-# Booleans for ranges, species
-ranges = config.getboolean('RANGE SPECS', 'ranges')
-
-# get the range bounds
-range_bnds = np.asarray(parse_range(config, 'RANGE SPECS', 'range_bounds'))
-
-for species in species_info.keys():
-	''' Check if species are enabled.
-	This will get the 'enabled' and 'range_bounds' keys for each species
-	from the config file. If species are not found, they are set to False'''
-	# read from file
-	enabled = config.getboolean('SPECIES', species, fallback=False)
-	species_range = parse_range(config, 'RANGE SPECS', str(species + '_range'))
-
-	# set values in dictionary
-	species_info[species]['enabled'] = enabled
-	species_info[species]['range_bounds'] = species_range
-
-	# check if species bounds are == range bounds
-	if any(i not in range_bnds for i in species_range):
-		sys.exit('ERROR: Species range bound not equal to range bounds')
+# INPUT
+filepath = 'config_new.ini' # os.path.join(config)
+config = configparser.ConfigParser()
+config.read(filepath)
 
 # ==============================================================================
-# Calculate bins with Volume ratio approach
+# Initialize
 # ==============================================================================´
 
-# Bin calculations
-# calculate volume ratio
-V_rat = (r_N / r_1) ** (3 / (N-1))
+# initialize bins
+bin_specs = BinSpecs(config)
+bin_specs.calc_bins()
 
-# calculate smallest volume
-v_0 = 3/4 * pi * (r_1) ** 3 # smallest
+# initialize ranges
+range_specs = RangeSpecs(config)
 
-# calculate all volumes for bins
-v = v_0 * V_rat ** np.arange(N)
+# initialize aerosol species
+species_obj_list = []
+for section in config.sections():
+    if section not in ['BIN SPECS', 'RANGE SPECS']:
+        species_obj_list.append(AerosolSpecies(config, section))
 
-# calculate radii
-r = (v*4/(3*pi))**(1/3)
+[species_obj.check_range_bnds for species_obj in species_obj_list]  # check species range bnds
 
-# volume widths / bin bounds
-del_v = (2 * v * (V_rat - 1)) / (1+V_rat)
+# adjust range range_bnds
+range_specs.adjust_range_bnds(bin_specs.r_bnds)
 
-# lower radius bounds
-v_lo = (2*v) / (1+V_rat)
-
-# upper bnd for largest bin
-v_hi = V_rat*v_lo[-1]
-
-# append to get volume bounds
-v_bnds = np.append(v_lo, v_hi)
-
-# calculate radii from volume
-r_bnds = (v_bnds*4/(3*pi))**(1/3)
-
-# ==============================================================================
-# Set range bounds to the closest bin bounds to avoid a messy overlap
-# ==============================================================================
-
-# find bin bounds closest to range_bnds
-if ranges:
-	range_bnds[0] = r_bnds[0]
-	range_bnds[-1] = r_bnds[-1]
-	for i in range(1, len(range_bnds)-1):
-		for j in range(len(r_bnds)):
-   	     # check if range bnd is between adjacent range bnds
-			if range_bnds[i] > r_bnds[j] and range_bnds[i] < r_bnds[j+1]:
-			# chose the closest bnd
-				if abs(r_bnds[j] - range_bnds[i]) < abs(r_bnds[j+1] - range_bnds[i]):
-				# replace the value in range_bnds with the closest value
-					range_bnds[i] = r_bnds[j]
-				else:
-					range_bnds[i] = r_bnds[j+1]
-					break
-else:
-	# If no ranges (classic sectional scheme), set range bounds equal to bin bounds
-	range_bnds = r_bnds
-
-# ==============================================================================
-# set species bounds equal to range bounds
-# also the keys for what range indices each species has are set
-# ==============================================================================
-
-for species in species_info.keys():
-	''' The species bounds are adjusted to the range bounds'''
-	idx0 = (np.abs(range_bnds - species_info[species]['range_bounds'][0])).argmin()
-	idx1 = (np.abs(range_bnds - species_info[species]['range_bounds'][1])).argmin()
-	species_info[species]['range_bounds'] = np.asarray([range_bnds[idx0], range_bnds[idx1]])
-
-spec_list = []
-for species in species_info.keys():
-	if species_info[species]['enabled']:
-		spec_list.extend([f"{species_info[species]['short_name']}_R{i}"
-					for i in range(1, len(range_bnds))
-					if range_bnds[i] > species_info[species]['range_bounds'][0]
-					and range_bnds[i] <= species_info[species]['range_bounds'][1]])
-		species_info[species]['range_idx'].extend([i
-					for i in range(1, len(range_bnds))
-					if range_bnds[i] > species_info[species]['range_bounds'][0]
-					and range_bnds[i] <= species_info[species]['range_bounds'][1]])
-
-spec_array = np.array(spec_list)
-nspec = len(spec_array)
-ntrac = nspec + N
+# get species range indices
+[species_obj.get_range_idx(range_specs.range_bnds) for species_obj in species_obj_list]
 
 # =====================================================================
 # Write to namelist
@@ -213,27 +116,28 @@ ntrac = nspec + N
 # Write tracers instead to chem_mech file or my_chem_mech.in in case dir?
 # =====================================================================
 
-f = open("bin_nl", "w") # change to "a" later!!! -> atm_in?
+# XXX ADD ADDITIONAL AEROSOL INFO
+
+f = open("bin_nl", "w") # change to "a" later!!!
 f.write("&oslo_sectional_nl\n")
-f.write("nbin = " + str(N) + "\n")
-f.write("nrange = " + str(len(range_bnds)-1) + "\n")
+f.write("nbin = " + str(bin_specs.N) + "\n")
+f.write("nrange = " + str(len(range_specs.range_bnds)-1) + "\n")
 f.write("nspec = " + str(nspec) + "\n")
-f.write("ntrac = " + str(ntrac) + "\n")
 f.write("bin_list = ")
-for i in range(len(r)):
-	f.write(str(r[i]) + ",")
+for i in range(len(bin_specs.r)):
+	f.write(str(bin_specs.r[i]) + ",")
 f.write("\n")
 f.write("bin_bnd_list = ")
-for i in range(len(r_bnds)):
-	f.write(str(r_bnds[i]) + ",")
+for i in range(len(bin_specs.r_bnds)):
+	f.write(str(bin_specs.r_bnds[i]) + ",")
 f.write("\n")
 f.write("range_bnd_list = ")
-for i in range(len(range_bnds)):
-	f.write(str(range_bnds[i]) + ",")
+for i in range(len(range_specs.range_bnds)):
+	f.write(str(range_specs.range_bnds[i]) + ",")
 f.write("\n")
 f.write("bin_name_list = ")
-for i in range(1, len(r)+1):
-	f.write("'num_" + str(i) + "',")
+for i in range(1, len(bin_specs.r)+1):
+	f.write("'aer_" + str(i) + "',")
 f.write("\n")
 f.write("species_name_list = ")
 for i in range(len(spec_array)):
@@ -252,26 +156,29 @@ f.close()
 # "Solution" and "Implicit". Below these, the composition of the tracers and the
 # names of the tracers are added. The file is then written out to my_chem_mech.in
 # ==============================================================================
+# write my_chem_mech.in
 
+# write composition
 composition_list = []
 implicit_list = []
 
-for species in species_info.keys():
-   for i in range(len(species_info[species]['range_idx'])):
-      composition_list.append(
-         species_info[species]['short_name'] +
-         '_R' +
-         str(species_info[species]['range_idx'][i]) +
-         ' -> ' +
-         species_info[species]['composition']
-	  )
-      implicit_list.append(
-         species_info[species]['short_name'] +
-         '_R' +
-         str(species_info[species]['range_idx'][i])
-	  )
+for species in species_obj_list:
+	if species.active:
+		for i in range(len(species.range_idx)):
+			composition_list.append(
+				species.short_name +
+				'_R' +
+				str(species.range_idx[i]) +
+				' -> ' +
+				species.composition
+				)
+			implicit_list.append(
+				species.short_name +
+				'_R' +
+				str(species.range_idx[i])
+				)
 
-for i in range(1,N+1):
+for i in range(1,bin_specs.N+1):
    composition_list.append(
       'num_' + str(i) + ' -> H'
     )
@@ -280,10 +187,13 @@ for i in range(1,N+1):
    )
 
 
+
 # read chem_mech.in
 
 with open('chem_mech.in', 'r') as chem_file:
    lines = chem_file.readlines()
+
+modified_chem = []
 
 modified_chem = []
 
@@ -298,7 +208,6 @@ for line in lines:
 	if 'Implicit' in line and not 'End' in line:
 		for i in range(len(implicit_list)):
 			modified_chem.append(implicit_list[i] + '\n')
-#      modified_lines.append()
 # write out to my_chem_mech.in
 with open('my_chem_mech.in', 'w') as file:
       file.writelines(modified_chem)
