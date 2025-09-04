bf25963d60631bab8d673f2c8a62ba38283147b0
Trudigard <christina.brodowsky@gmail.com>
2025-09-04 14:46:08 +0200

    memory usage?

--- src/aero_model.F90
+++ src/aero_model.F90
@@ -110,11 +110,18 @@ contains
     use phys_control,   only: phys_getopts
     use dust_model,     only: dust_init
     use mo_setsox,   only : setsox, has_sox
+    use shr_mem_mod,      only: shr_mem_getusage
+    use mpi,              only: MPI_REAL8, MPI_MAX
+      use spmd_utils,      only: mpicom, masterprocid, masterproc
 
     !use aer_drydep_mod, only: inidrydep
     !use wetdep,         only: wetdep_init
 
     !use oslo_aero_ocean, only: oslo_aero_ocean_init ! TODO: DMS, add to build-namelist and chemistry.F90 and as well
+   logical :: calc_memory_increase = .true.
+    real(r8)                            :: mem_hw_beg, mem_hw_end
+    real(r8)                            :: mem_beg, mem_end
+      real(r8)                            :: temp ! For MPI
 
     ! args
     type(physics_buffer_desc), pointer :: pbuf2d(:,:)
@@ -130,7 +137,9 @@ contains
     character(len=*), intent(in) :: nlfile
 
     !call oslo_aero_ocean_init() ! DMS
-
+!    if (calc_memory_increase) then
+       call shr_mem_getusage(mem_hw_beg, mem_beg)
+!    end if
     ! TODO: fix this :)
     call phys_getopts( history_aerosol_out   = history_aerosol, &
                        history_dust_out      = history_dust,    &
@@ -163,6 +172,24 @@ contains
        endif
     endif
 
+ !     if (calc_memory_increase) then
+    temp = 0.0
+    call shr_mem_getusage(mem_hw_end, mem_end)
+         temp = mem_end - mem_beg
+         call MPI_reduce(temp, mem_end, 1, MPI_REAL8, MPI_MAX, masterprocid,  &
+              mpicom, ierr)
+         if (masterproc) then
+            write(iulog, *) subname, ': Increase in memory usage = ',    &
+                 mem_end, ' (MB)'
+         end if
+         temp = mem_hw_end - mem_hw_beg
+         call MPI_reduce(temp, mem_hw_end, 1, MPI_REAL8, MPI_MAX,             &
+              masterprocid, mpicom, ierr)
+         if (masterproc) then
+            write(iulog, *) subname, 'Increase in memory highwater = ',       &
+                 mem_hw_end, ' (MB)'
+         end if
+ !     end if
     ! TODO add aq chem (if has_sox ...)
 
     ! call aero_wetdep_init()
