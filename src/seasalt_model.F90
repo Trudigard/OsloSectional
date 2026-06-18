e6c8069f4206d3c23a319317c68bd81f09cc561b
Trudigard <christina.brodowsky@gmail.com>
2026-06-18 11:57:04 +0200

    fix seasalt_emis function

--- src/seasalt_model.F90
+++ src/seasalt_model.F90
@@ -16,17 +16,16 @@ module seasalt_model
   private
 
   ! public variables
-  public :: seasalt_active
+  public :: seasalt_specprop_ndx
 
   ! public procedures
   public :: seasalt_init
   public :: seasalt_emis
 
   ! initialize public
-  logical :: seasalt_active = .false.
 
   ! module variables
-  integer :: seasalt_specprop_ndx
+  integer :: seasalt_specprop_ndx = 0
 
   contains
 
@@ -53,7 +52,6 @@ module seasalt_model
         type = aero_props%spectype(ispecprop)
 
         if (trim(type) == 'seasalt') then
-            seasalt_active = .true.
             seasalt_specprop_ndx = ispecprop
         end if
     end do
@@ -64,19 +62,17 @@ module seasalt_model
   !=============================================================================
   subroutine seasalt_emis( u10cubed,  srf_temp, ocnfrc, ncol, cflx, aero_props )
 ! TODO: https://acp.copernicus.org/articles/11/4587/2011/acp-11-4587-2011.pdf
+
     ! dummy arguments
-    real(r8), intent(in) :: u10cubed(:)
-    real(r8), intent(in) :: srf_temp(:)
-    real(r8), intent(in) :: ocnfrc(:)
-    integer,  intent(in) :: ncol
+    real(r8), intent(in)    :: u10cubed(:)
+    real(r8), intent(in)    :: srf_temp(:)
+    real(r8), intent(in)    :: ocnfrc(:)
+    integer,  intent(in)    :: ncol
     real(r8), intent(inout) :: cflx(:,:)
+
     type(sectional_aerosol_properties), intent(in) :: aero_props
 
-    ! local vars
-    character(len=*), parameter :: subname = 'seasalt_emis'
-
- ! from CARMA: carma_model_mod.F90 /src/physics/carma/models/sea_salt
-  !--------CMS (Clarke, Monahan, and Smith source function)-------
+    !--------CMS (Clarke, Monahan, and Smith source function) from CARMA: carma_model_mod.F90 /src/physics/carma/models/sea_salt/carma_model_mod.F90
 
     ! ------------------------------------------------------------
     ! ----  Clarke Source Function. Coefficients for Ai    -------
@@ -99,19 +95,9 @@ module seasalt_model
     real(r8), parameter :: beta33 = 1.218e2_r8
     real(r8), parameter :: beta43 =-1.213e1_r8
     real(r8), parameter :: beta53 = 4.514e-1_r8
-    real(r8)            :: A1                                ! Coefficient Ak in Clarkes's source function
+    real(r8)            :: A1                                 ! Coefficient Ak in Clarkes's source function
     real(r8)            :: A2
     real(r8)            :: A3
-    real(r8)            :: wcap                         ! whitecap coverage
-    real(r8)            :: rpdry,rpdry_cm                              ! dry radius
-    real(r8)            :: Monahan, Clarke, Smith             ! dF/dr [#/m2/s/um]
-    real(r8), allocatable :: bin_ndx(:)
-    real(r8), allocatable :: bin_centers(:)
-    integer             :: ibin, icol
-    real(r8)            :: B_mona                            ! the parameter used in Monahan
-    real(r8)            :: r80, r80cm
-    real(r8)            :: ncflx                              ! dF/dr [#/m2/s/um]
-    real(r8), parameter :: xkar = 0.4_r8                      ! Von Karman constant
 
     ! --------------------------------------------------------------------
     ! ---- constants in calculating the particle wet radius [Gerber, 1985]
@@ -121,13 +107,16 @@ module seasalt_model
     real(r8), parameter :: c3   = 2.573e-11_r8     ! .
     real(r8), parameter :: c4   = -1.424_r8        ! constants in calculating the particle wet radius
 
-    real(r8)            :: u14, ustar_smith, cd_smith         ! 14m wind velocity, friction velocity and drag
     ! ---------------------------------------------
     ! coefficient A1, A2 in Andreas's Source funcion
     ! ---------------------------------------------
     real(r8)            ::A1A92
-    real(r8)            ::A2A92
-                                                              ! coefficient as desired by Andreas source function
+    real(r8)            ::A2A92                         ! coefficient as desired by Andreas source function
+
+    ! ---------------------------------------------
+    ! Parameter in Monahan
+    ! ---------------------------------------------
+    real(r8)            :: B_mona
 
     ! ---------------------------------------------
     ! coefficient in Smith's Source funcion
@@ -137,42 +126,77 @@ module seasalt_model
     real(r8), parameter ::  r1 = 2.1_r8
     real(r8), parameter ::  r2 = 9.2_r8
 
- !   if (.not. seasalt_active) return
+    ! Fluxes for Clarke, Monahan, Smith
+    real(r8)            :: Monahan, Clarke, Smith             ! dF/dr [#/m2/s/um]
+    real(r8)            :: ncflx                              ! dF/dr [#/m2/s/um]
+
+    ! Local
+    real(r8)            :: wcap                               ! whitecap coverage
+    real(r8)            :: rpdry, rpdry_cm                    ! dry radius
+    real(r8)            :: r80, r80cm
+    real(r8), allocatable :: bin_centers(:)
+    real(r8)            :: bin_width_um
+    real(r8), parameter :: xkar = 0.4_r8                      ! Von Karman constant
+    real(r8)            :: u14, cd_smith         ! 14m wind velocity, friction velocity and drag
+    real(r8), allocatable :: cflx_tmp(:,:)
+    integer             :: sslt_bin
+    ! running variables
+    integer             :: ibin, icol, irange
+
+    character(len=*), parameter :: subname = 'seasalt_emis'
+    ! -----------------------------------------------------------------------------------
+    ! -----------------------------------------------------------------------------------
+
+
+    if (.not. aero_props%is_active('seasalt')) return
 
 ! -> get q-array indices for emissions from aero_props%sped_bin_q_ndx(ispec, spec_bin_ndx) and spec_mmr_q_ndx(ispec, spec_range_ndx)
 
-
-    allocate(bin_ndx(aero_props%spec_nbin(seasalt_specprop_ndx)))
     allocate(bin_centers(aero_props%nbins()))
-          ! Add any surface flux here.
-          ncflx       = 0.0_r8
-          Monahan     = 0.0_r8
-          Clarke      = 0.0_r8
-          Smith       = 0.0_r8
+    allocate(cflx_tmp(pcols, aero_props%spec_nbin(spectype='seasalt')))
+    cflx_tmp    = 0._r8
 
-    bin_ndx = aero_props%spec_bin_ndx(seasalt_specprop_ndx, aero_props%spec_nbin(seasalt_specprop_ndx))
-    bin_centers = aero_props%bin_centers(aero_props%nbins())
+    bin_centers = aero_props%bin_centers(aero_props%nbins()) * 1.e6_r8 ! to um
 
-   ! do ibin = seasalt_bin_start, seasalt_bin_end
-    do ibin = bin_ndx(1), bin_ndx(aero_props%spec_nbin(seasalt_specprop_ndx))
-! TODO: check unit of rpdry
+    sslt_bin = 0
+    do ibin = aero_props%spec_bin_ndx(seasalt_specprop_ndx, 1), aero_props%spec_bin_ndx(seasalt_specprop_ndx, aero_props%spec_nbin(spectype='seasalt'))
+        sslt_bin = sslt_bin + 1
+        rpdry = bin_centers(ibin)               ! [um]
+        rpdry_cm = rpdry * 1.e-4_r8             ! [cm]
+! todo: get 80% RH radius differently?
+        r80cm   = (c1 *  (rpdry_cm) ** c2 / (c3 * rpdry_cm ** c4 - log10(0.8_r8)) + (rpdry_cm)**3._r8) ** (1._r8/3._r8) ! [cm]
+        r80     = r80cm *1.e4_r8                ! [um]
+
+        bin_width_um = (aero_props%bin_bounds(ibin, 2) - aero_props%bin_bounds(ibin, 1)) * 1.e6_r8 ! bin width in [um]
+
+        A1 = beta01 + beta11*(2._r8*rpdry) + beta21*(2._r8*rpdry)**2 + &
+                beta31*(2._r8*rpdry)**3 + beta41*(2._r8*rpdry)**4 + beta51*(2._r8*rpdry)**5
+        A2 = beta02 + beta12*(2._r8*rpdry) + beta22*(2._r8*rpdry)**2 + &
+                 beta32*(2._r8*rpdry)**3 + beta42*(2._r8*rpdry)**4 + beta52*(2._r8*rpdry)**5
+        A3 = beta03 + beta13*(2._r8*rpdry) + beta23*(2._r8*rpdry)**2 + &
+                 beta33*(2._r8*rpdry)**3 + beta43*(2._r8*rpdry)**4 + beta53*(2._r8*rpdry)**5
+
 
         do icol = 1, ncol
+
+        ! Initialize surface fluxes
+            ncflx       = 0.0_r8
+            Monahan     = 0.0_r8
+            Clarke      = 0.0_r8
+            Smith       = 0.0_r8
+
+            ! white cap fraction
             wcap = 3.84e-6_r8 * u10cubed(icol)      ! in percent, ie., 75%, wcap = 0.75
-!    W(:ncol)=3.84e-6_r8*u10cubed(:ncol)*0.1_r8 ! whitecap area
 
-            rpdry = bin_centers(ibin) ! in m
-            rpdry_cm = rpdry * 100._r8
-
-            r80cm   = (c1 *  (rpdry_cm) ** c2 / (c3 * rpdry_cm ** c4 - log10(0.8_r8)) + (rpdry_cm)**3._r8) ** (1._r8/3._r8) ! [cm]
-            r80     = r80cm *1.e4_r8    ! [um]
-
-            A1 = beta01 + beta11*(2._r8*rpdry) + beta21*(2._r8*rpdry)**2 + &
-                beta31*(2._r8*rpdry)**3 + beta41*(2._r8*rpdry)**4 + beta51*(2._r8*rpdry)**5
-            A2 = beta02 + beta12*(2._r8*rpdry) + beta22*(2._r8*rpdry)**2 + &
-                 beta32*(2._r8*rpdry)**3 + beta42*(2._r8*rpdry)**4 + beta52*(2._r8*rpdry)**5
-            A3 = beta03 + beta13*(2._r8*rpdry) + beta23*(2._r8*rpdry)**2 + &
-                 beta33*(2._r8*rpdry)**3 + beta43*(2._r8*rpdry)**4 + beta53*(2._r8*rpdry)**5
+            !****************************************
+            !        Hoppel correction factor
+            !        Smith drag coefficients and etc
+            !****************************************
+            if (u10cubed(icol) .le. (10._r8)**3.41_r8) then
+                cd_smith = 1.14e-3_r8
+            else
+                cd_smith = (0.49_r8 + 0.065_r8 * u10cubed(icol)) * 1.e-3_r8
+            end if
 
 ! TODO: replaced rdry with rpdry here
             !Clarke
@@ -203,7 +227,7 @@ module seasalt_model
             !%%%%%%%%%%%%%%%%%%%%%%%%%
   !          if (rdry .lt. 0.1_r8) then   ! originally cut at 0.1 um
             ! ***CMS1*****
-            if (rpdry .lt. 1e-6_r8) then    ! cut at 1.0 um, rpdry in [m]
+            if (rpdry .lt. 1._r8) then    ! cut at 1.0 um
             ! ***CMS2*****
   !          if (rdry .lt. 2._r8) then    ! cut at 2.0 um
                 ncflx = Clarke
@@ -219,18 +243,28 @@ module seasalt_model
                 end if
             end if
 
-            !%%%%%%%%%%%%%%%%%%%%%%%%%
             ! Apply Hoppel correction
-            !%%%%%%%%%%%%%%%%%%%%%%%%%
-!            ncflx = ncflx * fref
-!          end if
+!            ncflx = ncflx * fref ! fref is = 1._r8 in CARMA
 
           ! convert ncflx [#/m^2/s/um] to surfaceFlx [kg/m^2/s]
-!          surfaceFlux(icol) = ncflx * dr(ibin) * rmass(ibin) * 10._r8      ! *1e4[um/cm] * 1.e-3[kg/g]
 
-          ! weighted by the ocean fraction
-!          surfaceFlux(icol) = surfaceFlux(icol) * cam_in%ocnfrac(icol)
+            ! Number flux
+            cflx_tmp(icol, sslt_bin) = ncflx * bin_width_um * ocnfrc(icol)  ! [#/m2/s]
 
+            cflx(icol, aero_props%spec_bin_q_ndx(seasalt_specprop_ndx, sslt_bin)) = cflx(icol, aero_props%spec_bin_q_ndx(seasalt_specprop_ndx, sslt_bin)) &
+                                                                                + cflx_tmp(icol, sslt_bin)
+            ! mass flux
+
+            do irange = 1, aero_props%spec_nrange(seasalt_specprop_ndx)
+                if (aero_props%bins2ranges(aero_props%spec_bin_ndx(seasalt_specprop_ndx, sslt_bin)) == aero_props%spec_range_ndx(seasalt_specprop_ndx, irange)) then
+
+                cflx(icol, aero_props%spec_mmr_q_ndx(seasalt_specprop_ndx, irange)) = &
+                                                                                    cflx(icol, aero_props%spec_mmr_q_ndx(seasalt_specprop_ndx, irange)) &
+                                                                                  + cflx_tmp(icol,sslt_bin) &
+                                                                                  * aero_props%density(seasalt_specprop_ndx) &
+                                                                                  * aero_props%particle_volume(aero_props%spec_bin_ndx(seasalt_specprop_ndx, sslt_bin))
+                end if
+            end do
         end do
 
     end do
